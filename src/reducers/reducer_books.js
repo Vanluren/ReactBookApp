8a767de7912cb98a610141319241bd7ea755dbe0
Vanluren <villadsvalur@gmail.com>
2017-03-02 09:20:47 +0100

    Initial commit, bruger nu Yarn i stedet for NPM

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+  return [
+    { title: 'Javascript: The good parts' },
+    { title: 'Harry Potter and the Order of the Phoenix' },
+    { title: 'Lord of the Rings: Fellowship of the Ring' },
+    { title: 'Lord of the Rings: the Two Towers' },
+    { title: 'Lord of the Rings: the Return of the King' },
+    { title: 'The Hobbit' },
+  ],
+}
